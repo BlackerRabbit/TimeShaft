71b5f7d87b25c3b0fe71b4cf4457425e464f600f
jyw <372173813@qq.com>
2016-11-17 11:11:22 +0800

    init

---
+++ TimeShaft/TimeShaft/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TimeShaft
+//
+//  Created by 蒋正峰 on 2016/11/17.
+//  Copyright © 2016年 蒋正峰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
